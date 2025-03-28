52dcc51db9b7abe246761c996eb30561ed051135
Mahnoor-Farrukh <mahnoorfarrukh05@gmail.com>
2025-03-28 20:30:28 +0500

    Add files via upload

---
+++ WAD Lab 7/Task1.js
@@ -0,0 +1,9 @@
+
+let heading = document.getElementById("heading");
+
+
+let name = "Mahnoor";  
+let regNo = "52926"; 
+
+
+heading.innerHTML = heading.innerHTML + " - Name: " + name   + " - Reg No: " + regNo ;
---
+++ WAD Lab 7/Task2.js
@@ -0,0 +1,5 @@
+let divs = document.querySelectorAll(".Container");
+
+        for (let i = 0; i < 3; i++) {
+            divs[i].innerText ;
+        }
